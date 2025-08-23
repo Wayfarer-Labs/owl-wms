b95f041f8f8358a8d53057f4b43772205629f44c
Andrew Lapp <andrew@github.rew.la>
2025-08-23 14:52:34 -0400

    use timestep space, not sigma space

--- owl_wms/sampling/schedulers.py
+++ owl_wms/sampling/schedulers.py
@@ -2,21 +2,21 @@ from diffusers import FlowMatchEulerDiscreteScheduler, UniPCMultistepScheduler
 import torch
 
 
-def get_sd3_euler(n_steps):
+def get_sd3_euler(n_steps, num_train_timesteps=1000):
+    # TODO: keep old fn for backwards compatability, rename to def get_scheduler_dts
     scheduler = UniPCMultistepScheduler(
         prediction_type="flow_prediction",
         use_flow_sigmas=True,
         flow_shift=3.0,
-        num_train_timesteps=1000,
+        num_train_timesteps=num_train_timesteps,  # granularity
     )
     scheduler.set_timesteps(num_inference_steps=n_steps)
 
-    # t = scheduler.timesteps.float() / float(scheduler.config.num_train_timesteps - 1)
-    # dt = t - torch.cat([t[1:], t.new_zeros(1)])
+    t = scheduler.timesteps.float() / float(num_train_timesteps - 1)
+    t_next = torch.cat([t[1:], t.new_zeros(1)])
+    dt = t - t_next
+    return dt
 
-    sigmas = scheduler.sigmas.float()
-    d_sigma = sigmas[:-1] - sigmas[1:]
-    return d_sigma
 
 if __name__ == "__main__":
     scheduler = get_sd3_euler(10)
