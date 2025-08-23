1703821a4beb2ff05959e4942a4e5329286b6c0f
Andrew Lapp <andrew@github.rew.la>
2025-08-23 14:14:22 -0400

    clean sigmas

--- owl_wms/sampling/schedulers.py
+++ owl_wms/sampling/schedulers.py
@@ -14,11 +14,9 @@ def get_sd3_euler(n_steps):
     # t = scheduler.timesteps.float() / float(scheduler.config.num_train_timesteps - 1)
     # dt = t - torch.cat([t[1:], t.new_zeros(1)])
 
-    sigmas = scheduler.sigmas.to(torch.float32)  # populated by set_timesteps
-    dt_sigma = sigmas - torch.cat([sigmas[1:], sigmas.new_zeros(1)])  # Δσ_i = σ_i - σ_{i+1}
-
-    return dt_sigma
-
+    sigmas = scheduler.sigmas.float()
+    d_sigma = sigmas[:-1] - sigmas[1:]
+    return d_sigma
 
 if __name__ == "__main__":
     scheduler = get_sd3_euler(10)
