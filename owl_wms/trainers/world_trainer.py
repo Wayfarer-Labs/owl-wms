8509991da80bc4d84a61b500b98c4b6461dd497e
Andrew Lapp <andrew@github.rew.la>
2025-08-27 22:16:07 -0400

    fix

--- owl_wms/trainers/world_trainer.py
+++ owl_wms/trainers/world_trainer.py
@@ -398,7 +398,7 @@ class WorldTrainer(BaseTrainer):
         video_out, controller_inputs = map(self._gather_concat_cpu, (video_out, controller_inputs))
 
         if self.rank == 0:
-            mouse, btn = _eval_batch["mouse"], _eval_batch["btn"]
+            mouse, btn = _eval_batch["mouse"], _eval_batch["buttons"]
             eval_wandb_dict = to_wandb_samples(video_out, mouse, btn, fps=60)
         else:
             eval_wandb_dict = None
