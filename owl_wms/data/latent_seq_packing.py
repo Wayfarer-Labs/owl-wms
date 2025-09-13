d55b98043863701d7a1bb2b8c3a28a83ba10f32c
Andrew Lapp <andrew@github.rew.la>
2025-09-13 14:05:59 -0400

    60fps, 30fps, 20fps train

--- owl_wms/data/latent_seq_packing.py
+++ owl_wms/data/latent_seq_packing.py
@@ -175,7 +175,7 @@ def collate_fn(batch, batch_columns: list, latent_column: str | None = None):
 
 def get_loader(
         batch_size, dataset_path, seq_len, batch_columns, latent_column=None,
-        sampling_periods: tuple[int, ...] = (1,)
+        sampling_periods: tuple[int, ...] = (1, 2, 3,)
 ):
     assert batch_size == 1
 
