ff8aaac946d3170da8f14624a441f7044ff074d9
Andrew Lapp <andrew@github.rew.la>
2025-09-23 17:43:31 -0400

    revert to original MLP

--- owl_wms/nn/mlp.py
+++ owl_wms/nn/mlp.py
@@ -1,24 +1,20 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
-from .quantized_linear import QLinear
 
 
 class MLPCustom(nn.Module):
-    def __init__(self, dim_in, dim_middle, dim_out, fp8_mode=False):
+    def __init__(self, dim_in, dim_middle, dim_out):
         super().__init__()
 
-        Linear = QLinear if fp8_mode else nn.Linear
-        self.fc1 = Linear(dim_in, dim_middle)
-        self.fc2 = Linear(dim_middle, dim_out)
+        self.fc1 = nn.Linear(dim_in, dim_middle)
+        self.fc2 = nn.Linear(dim_middle, dim_out)
 
         nn.init.kaiming_normal_(self.fc1.weight)
         nn.init.kaiming_normal_(self.fc2.weight)
 
         self.fc1.weight.data *= dim_in ** -0.5
-        if fp8_mode:
-            nn.init.zeros_(self.fc2.weight)
-        else:
-            self.fc2.weight.data *= dim_middle ** -0.5
+        self.fc2.weight.data *= dim_middle ** -0.5
 
         nn.init.zeros_(self.fc1.bias)
         nn.init.zeros_(self.fc2.bias)
@@ -31,13 +27,8 @@ class MLPCustom(nn.Module):
 
 
 class MLP(MLPCustom):
-    def __init__(self, config):
-        super().__init__(
-            config.d_model,
-            config.d_model * getattr(config, "mlp_ratio", 4),
-            config.d_model,
-            fp8_mode=getattr(config, "mlp_fp8", False),
-        )
+    def __init__(self, config : 'TransformerConfig'):
+        super().__init__(config.d_model, config.d_model * 4, config.d_model)
 
     def forward(self, x):
         x = self.fc1(x)
