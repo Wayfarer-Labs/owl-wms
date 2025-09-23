de8b22f3439884a84e130c54375a186cad953f34
Andrew Lapp <andrew@github.rew.la>
2025-09-23 17:01:16 -0400

    fp8 + zero init MLP out

--- owl_wms/nn/mlp.py
+++ owl_wms/nn/mlp.py
@@ -7,18 +7,18 @@ class MLPCustom(nn.Module):
     def __init__(self, dim_in, dim_middle, dim_out, fp8_mode=False):
         super().__init__()
 
-        if fp8_mode:
-            self.fc1 = QLinear(dim_in, dim_middle)
-            self.fc2 = QLinear(dim_middle, dim_out)
-        else:
-            self.fc1 = nn.Linear(dim_in, dim_middle)
-            self.fc2 = nn.Linear(dim_middle, dim_out)
+        Linear = QLinear if fp8_mode else nn.Linear
+        self.fc1 = Linear(dim_in, dim_middle)
+        self.fc2 = Linear(dim_middle, dim_out)
 
         nn.init.kaiming_normal_(self.fc1.weight)
         nn.init.kaiming_normal_(self.fc2.weight)
 
         self.fc1.weight.data *= dim_in ** -0.5
-        self.fc2.weight.data *= dim_middle ** -0.5
+        if fp8_mode:
+            nn.init.zeros_(self.fc2.weight)
+        else:
+            self.fc2.weight.data *= dim_middle ** -0.5
 
         nn.init.zeros_(self.fc1.bias)
         nn.init.zeros_(self.fc2.bias)
