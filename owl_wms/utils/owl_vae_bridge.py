d8a4409c8818db1b5db54aec16f7750f0d4c4a9d
Andrew Lapp <andrew@github.rew.la>
2025-09-26 12:32:06 -0400

    dont use mit-han-lab/dc-ae-f64c128-mix-1.0-diffusers when vae_id is None

--- owl_wms/utils/owl_vae_bridge.py
+++ owl_wms/utils/owl_vae_bridge.py
@@ -20,22 +20,17 @@ def _get_decoder_only():
     return model
 
 def get_decoder_only(vae_id, cfg_path, ckpt_path):
-        if vae_id == "dcae":
-            model_id = "mit-han-lab/dc-ae-f64c128-mix-1.0-diffusers"
-            model = AutoencoderDC.from_pretrained(model_id).bfloat16().cuda().eval()
-            del model.encoder
-            return model.decoder
-        else:
-            cfg = Config.from_yaml(cfg_path).model
-            model = get_model_cls(cfg.model_id)(cfg)
-            try:
-                model.load_state_dict(torch.load(ckpt_path, map_location='cpu',weights_only=False))
-            except:
-                model.decoder.load_state_dict(torch.load(ckpt_path, map_location='cpu',weights_only=False))
-            del model.encoder
-            model = model.decoder
-            model = model.bfloat16().cuda().eval()
-            return model
+    assert vae_id is None
+    cfg = Config.from_yaml(cfg_path).model
+    model = get_model_cls(cfg.model_id)(cfg)
+    try:
+        model.load_state_dict(torch.load(ckpt_path, map_location='cpu',weights_only=False))
+    except:
+        model.decoder.load_state_dict(torch.load(ckpt_path, map_location='cpu',weights_only=False))
+    del model.encoder
+    model = model.decoder
+    model = model.bfloat16().cuda().eval()
+    return model
 
 @torch.no_grad()
 def make_batched_decode_fn(decoder, batch_size = 8):
