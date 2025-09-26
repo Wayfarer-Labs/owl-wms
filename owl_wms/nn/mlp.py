af45caf6751e6216d966a857c50bca54ed194306
Andrew Lapp <andrew@github.rew.la>
2025-09-25 23:19:56 -0400

    no bias relu mlp

--- owl_wms/nn/mlp.py
+++ owl_wms/nn/mlp.py
@@ -3,11 +3,11 @@ import torch.nn.functional as F
 
 
 class MLPCustom(nn.Module):
-    def __init__(self, dim_in, dim_middle, dim_out):
+    def __init__(self, dim_in, dim_middle, dim_out, bias=True):
         super().__init__()
 
-        self.fc1 = nn.Linear(dim_in, dim_middle)
-        self.fc2 = nn.Linear(dim_middle, dim_out)
+        self.fc1 = nn.Linear(dim_in, dim_middle, bias=bias)
+        self.fc2 = nn.Linear(dim_middle, dim_out, bias=bias)
 
         nn.init.kaiming_normal_(self.fc1.weight)
         nn.init.kaiming_normal_(self.fc2.weight)
@@ -15,8 +15,9 @@ class MLPCustom(nn.Module):
         self.fc1.weight.data *= dim_in ** -0.5
         self.fc2.weight.data *= dim_middle ** -0.5
 
-        nn.init.zeros_(self.fc1.bias)
-        nn.init.zeros_(self.fc2.bias)
+        if bias:
+            nn.init.zeros_(self.fc1.bias)
+            nn.init.zeros_(self.fc2.bias)
 
     def forward(self, x):
         x = self.fc1(x)
@@ -29,13 +30,10 @@ class MLP(MLPCustom):
     def __init__(self, config):
         dim_middle = config.d_model * getattr(config, "mlp_ratio", 4)
 
-        super().__init__(config.d_model, dim_middle, config.d_model)
+        super().__init__(config.d_model, dim_middle, config.d_model, bias=False)
 
         nn.init.kaiming_normal_(self.fc1.weight, mode='fan_in', nonlinearity='relu')
         nn.init.normal_(self.fc2.weight, mean=0.0, std=1.0 / dim_middle**0.5)
-
-        nn.init.zeros_(self.fc1.bias)
-        nn.init.zeros_(self.fc2.bias)
 
     def forward(self, x):
         x = self.fc1(x)
