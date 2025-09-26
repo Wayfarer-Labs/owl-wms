ffd59afff5576a95b7e6838f5dce1a69d754af07
Andrew Lapp <andrew@github.rew.la>
2025-09-26 11:00:25 -0400

    revert to silu, no bias

--- owl_wms/nn/mlp.py
+++ owl_wms/nn/mlp.py
@@ -1,38 +1,31 @@
-import torch
 from torch import nn
 import torch.nn.functional as F
 
 
 class MLPCustom(nn.Module):
-    def __init__(self, dim_in, dim_middle, dim_out, mlp_glu=False):
+    def __init__(self, dim_in, dim_middle, dim_out):
         super().__init__()
 
-        self.up_proj = nn.Linear(dim_in, dim_middle, bias=False)
-        self.down_proj = nn.Linear(dim_middle, dim_out, bias=False)
+        self.fc1 = nn.Linear(dim_in, dim_middle, bias=False)
+        self.fc2 = nn.Linear(dim_middle, dim_out, bias=False)
 
-        nn.init.xavier_uniform_(self.up_proj.weight)
-        nn.init.xavier_uniform_(self.down_proj.weight)
-        with torch.no_grad():
-            self.down_proj.weight.data *= dim_middle ** -0.5
+        nn.init.kaiming_normal_(self.fc1.weight)
+        nn.init.kaiming_normal_(self.fc2.weight)
 
-        self.mlp_glu = mlp_glu
-        if self.mlp_glu:
-            self.glu_proj = nn.Linear(dim_in, dim_middle, bias=False)
-            nn.init.xavier_uniform_(self.glu_proj.weight)
+        self.fc1.weight.data *= dim_in ** -0.5
+        self.fc2.weight.data *= dim_middle ** -0.5
 
     def forward(self, x):
-        if not self.mlp_glu:
-            x = F.silu(self.up_proj(x))
-        else:
-            x = F.silu(self.up_proj(x)) * self.glu_proj(x)
-        return self.down_proj(x)
+        x = self.fc1(x)
+        x = F.silu(x)
+        x = self.fc2(x)
+        return x
 
 
 class MLP(MLPCustom):
     def __init__(self, config):
         super().__init__(
             config.d_model,
-            int(config.d_model * getattr(config, "mlp_ratio", 4)),
-            config.d_model,
-            mlp_glu=True
+            config.d_model * getattr(config, "mlp_ratio", 4),
+            config.d_model
         )
