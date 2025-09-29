d9653dcc031a5613252238c6d19788b81aa28572
Andrew Lapp <andrew@github.rew.la>
2025-09-29 14:03:19 -0400

    use transformer engine DelayedScaling

--- owl_wms/trainers/world_trainer.py
+++ owl_wms/trainers/world_trainer.py
@@ -72,14 +72,17 @@ class WorldTrainer(BaseTrainer):
 
     @property
     def te_autocast_ctx(self):
-        from transformer_engine.common.recipe import Format, MXFP8BlockScaling
+        # import contextlib
+        # return contextlib.nullcontext()
+        from transformer_engine.common.recipe import Format, MXFP8BlockScaling, DelayedScaling
         from transformer_engine.pytorch import fp8_autocast
 
         # TODO: try the below
         # DelayedScaling(fp8_format=Format.HYBRID, amax_history_len=16, amax_compute_algo="most_recent", margin=1))
         # recipe.DelayedScaling()
 
-        return fp8_autocast(enabled=True, fp8_recipe=MXFP8BlockScaling(fp8_format=Format.E4M3))
+        # return fp8_autocast(enabled=True, fp8_recipe=MXFP8BlockScaling(fp8_format=Format.E4M3))
+        return fp8_autocast(enabled=True, fp8_recipe=DelayedScaling(fp8_format=Format.E4M3))
 
     @staticmethod
     def get_raw_model(model):
