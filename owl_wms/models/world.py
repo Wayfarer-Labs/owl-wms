17377482532c1d5c861a1157415539edc2858697
Andrew Lapp <andrew@github.rew.la>
2025-10-01 22:30:04 -0400

    unify patching

--- owl_wms/models/world.py
+++ owl_wms/models/world.py
@@ -188,12 +188,7 @@ class WorldModel(nn.Module):
         self.transformer = WorldDiT(config)
 
         self.patch = (ph, pw) = tuple(getattr(config, "patch", (1, 1)))
-
-        if self.patch == (1, 1):
-            self.proj_in = nn.Linear(config.channels, config.d_model, bias=False)
-        else:
-            self.proj_in = nn.Conv2d(config.channels, config.d_model, kernel_size=(ph, pw), stride=(ph, pw), bias=False)
-
+        self.proj_in = nn.Conv2d(config.channels, config.d_model, kernel_size=(ph, pw), stride=(ph, pw), bias=False)
         self.out_norm = owl_nn.AdaLN(config.d_model)
         self.proj_out = nn.Linear(config.d_model, config.channels * ph * pw, bias=True)
 
@@ -235,22 +230,15 @@ class WorldModel(nn.Module):
         cond = self.denoise_step_emb(sigma)  # [B, N, d]
         ctrl_emb = self.ctrl_emb(controller_inputs) if controller_inputs is not None else None
 
-        if self.patch == (1, 1):
-            x = eo.rearrange(x, 'b n c h w -> b (n h w) c')
-            x = self.proj_in(x)
-            x = self.transformer(x, pos_ids, cond, prompt_emb, ctrl_emb, doc_id, kv_cache, curr_frame_mask)
-            x = self.proj_out(F.silu(self.out_norm(x, cond)))
-            x = eo.rearrange(x, 'b (n h w) c -> b n c h w', h=H, w=W)
-        else:
-            # patchify
-            x = eo.rearrange(x, 'b n c h w -> (b n) c h w')
-            x = self.proj_in(x)
-            x = eo.rearrange(x, '(b n) d h w -> b (n h w) d', b=B, n=N)
-            # backbone fwd
-            x = self.transformer(x, pos_ids, cond, prompt_emb, ctrl_emb, doc_id, kv_cache, curr_frame_mask)
-            # unpatchify
-            x = self.proj_out(F.silu(self.out_norm(x, cond)))
-            x = eo.rearrange(x, 'b (n h w) (c ph pw) -> b n c (h ph) (w pw)', n=N, h=Hp, w=Wp, ph=ph, pw=pw)
+        # patchify
+        x = eo.rearrange(x, 'b n c h w -> (b n) c h w')
+        x = self.proj_in(x)
+        x = eo.rearrange(x, '(b n) d h w -> b (n h w) d', b=B, n=N)
+        # backbone fwd
+        x = self.transformer(x, pos_ids, cond, prompt_emb, ctrl_emb, doc_id, kv_cache, curr_frame_mask)
+        # unpatchify
+        x = self.proj_out(F.silu(self.out_norm(x, cond)))
+        x = eo.rearrange(x, 'b (n h w) (c ph pw) -> b n c (h ph) (w pw)', n=N, h=Hp, w=Wp, ph=ph, pw=pw)
 
         return x
 
