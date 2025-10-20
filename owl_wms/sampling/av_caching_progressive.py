a4364f5e07a7add5f770d543cfe884a01d4d96e5
Andrew Lapp <andrew@github.rew.la>
2025-10-19 21:23:48 -0400

    apply roll

--- owl_wms/sampling/av_caching_progressive.py
+++ owl_wms/sampling/av_caching_progressive.py
@@ -88,7 +88,7 @@ class AVCachingSampler:
             )
 
             # slide window of history frames
-            prev_rollouts = x.new_full((uncached_k, self.n_steps + 1, x.size(0), *x.shape[2:]), torch.nan)
+            prev_rollouts = torch.roll(prev_rollouts, shifts=-1, dims=0)
             prev_rollouts[-1] = new_rollout
 
             latents.append(x[:, -1:])
